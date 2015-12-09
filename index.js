e5d320d82f5e5c31ecf1cb1f44698b6238938f2c
Leah Steinberg <leahsteinberg1@gmail.com>
2015-12-09 16:09:33 -0500

    getting rid of socket stuff

--- index.js
+++ index.js
@@ -29,9 +29,6 @@ pserver.on('connection', function(id) {
 pserver.on('disconnect', function(id) {
 
   console.log("peer disconnected: ", id);
-
-
-
 });
 
 
@@ -39,21 +36,20 @@ var rooms = {names: {}, next_doc_id: 1, docs: {}};
 
 
 
-
+/* htpp end points */
 
 app.get('/:doc', function(req, res) {
   res.sendFile(path.resolve('public/index.html'));
-  // need to some how return the doc id and site id in here. 
+
 });
 
 app.get('/docs/:doc', function(req, res){
-  //res.send(JSON.stringify({"hi": req.params.doc}));
-  //console.log("request is is:", req.params);
+
   var doc_url = req.params.doc;
   var toReturn = {};
   if (rooms.names[doc_url] === undefined){
     /* make a new document */
-    console.log("new doc!");
+    console.log("new doc --> ", doc_url);
 
     var curr_site_id = 1;
     var curr_doc_id = rooms.next_doc_id;
@@ -72,10 +68,9 @@ app.get('/docs/:doc', function(req, res){
 
     rooms.next_doc_id = curr_doc_id + 1;
     new_doc.next_site_id = curr_site_id + 1;
-    console.log(rooms)
 
   } else {
-    console.log("new doc!");
+
     /* document already exists */
     var curr_doc_id = rooms.names[doc_url];
     var curr_doc = rooms.docs[curr_doc_id];
@@ -86,53 +81,8 @@ app.get('/docs/:doc', function(req, res){
     toReturn = {"doc_id": curr_doc_id, "site_id": curr_site_id, "members": curr_doc.members}
     curr_doc.members.push(curr_site_id);
   }
-  console.log("sending back", toReturn);
   res.send(JSON.stringify(toReturn))
   // send back to user the doc id 
-
 });
-
-/* logic to handle clients*/
-var siteCounter  = 1; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* socket io server*/
-var io = require('socket.io')(server);
-console.log("socket.io is working: ", io!= undefined);
-
-
-
-io.sockets.on('connection', function(socket){
-    /* new client */
-  
-    var idUpdate = [{"siteId": siteCounter, "type": "SiteId"}]
-    socket.emit("serverWUpdates", JSON.stringify(idUpdate));
-    //console.log('a user connected, siteId: ', siteCounter);
-
-    siteCounter = siteCounter + 1;
-
-    socket.on("localEdits", function (msg){
-    	wootUpdate = JSON.parse(msg);
-    	if (wootUpdate === undefined || wootUpdate.length === 0){
-    		return;
-    	}
-
-      //console.log("msg:", wootUpdate);
-      socket.broadcast.emit("serverWUpdates", wootUpdate);
-
-    });
-});
-
 
 
